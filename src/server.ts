d25ec58f20b903d39ee7c9c0c06f380c7debd1f3
unknown <viniciusbuenodeoliveira2017@gmail.com>
2024-12-30 16:35:36 -0300

    Validação de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
-app.get("/movies", async (_: Request, res: Response) => {
+app.get("/movies", async (_: Request, res: Response): Promise<void> => {
     try {
         const movies = await prisma.movie.findMany({
             orderBy: {
@@ -24,12 +24,23 @@ app.get("/movies", async (_: Request, res: Response) => {
     }
 });
 
-app.post("/movies", async (req: Request, res: Response) => {
+app.post("/movies", async (req: Request, res: Response): Promise<void> => {
     const { title, genre_id, language_id, oscar_count, release_date } =
         req.body;
 
     try {
-        await prisma.movie.create({
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: title, mode: "insensitive" } },
+        });
+
+        if (movieWithSameTitle) {
+            res.status(409).json({
+                message: "Já existe um filme cadastrado com esse título",
+            });
+            return;
+        }
+
+        const newMovie = await prisma.movie.create({
             data: {
                 title,
                 genre_id,
@@ -38,7 +49,11 @@ app.post("/movies", async (req: Request, res: Response) => {
                 release_date: new Date(release_date),
             },
         });
-        res.status(201).send({ message: "Filme cadastrado com sucesso" });
+
+        res.status(201).json({
+            message: "Filme cadastrado com sucesso",
+            movie: newMovie,
+        });
     } catch (err) {
         res.status(400).json({
             message: "Falha ao cadastrar um filme",
